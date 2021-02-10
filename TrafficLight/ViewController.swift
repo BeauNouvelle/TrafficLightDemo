7fa2b8eaa9d1bddf61acb52a1433883a3bb488c9
Beau Nouvelle <beau@getswifty.dev>
2021-02-10 19:09:01 +1100

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Beau Nouvelle on 10/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
